c36b80f2f649534a093caedc69f3c3c854a7b687
GiveMeFox <givemefoxlang@gmail.com>
2023-09-21 10:12:39 +0200

    completed

---
+++ Laboratorium1/app/src/main/java/cc/givemefox/laboratorium1/MainActivity.kt
@@ -0,0 +1,19 @@
+package cc.givemefox.laboratorium1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            Toast.makeText(applicationContext, "GiveMeFox", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
